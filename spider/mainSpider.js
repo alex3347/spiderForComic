529cfd624c0b8a0861f57f203e62e493597a0b04
alex <alex@alexdeMacBook-Pro.local>
2018-06-21 20:09:25 +0800

    add child_process

--- spider/mainSpider.js
+++ spider/mainSpider.js
@@ -3,6 +3,14 @@ let fs = require('fs');
 let cheerio = require('cheerio');
 let request = require('request');
 let url = "http://comic.kukudm.com/comiclist/3/3/1.htm";
+let execFileSync = require('child_process').execFileSync;
+
+execFileSync('phantomjs', ['phantomJsSpider.js'],function(error, result){
+    console.log('result');
+    process.on('message',function(m){
+        console.log(m)
+    })
+})
 
 function download(url,callback){
     http.get(url,function(res){
@@ -18,22 +26,23 @@ function download(url,callback){
       })
 }
 
-download(url,function (data) {  
-    if(data){
-        let $=cheerio.load(data);
-        // let imgs = $('img');
-        console.log($('window.server'));
-        console.log($('table').eq(1).find('script').eq(0).html());
-        // $('img').each(function (i,elem) {
-        //     let imgSrc = $(this).attr('src');
-        //     console.log(imgSrc,1);
-        //     if(imgSrc && i == 2){
-        //         if(imgSrc.indexOf('https') == -1){
-        //             imgSrc = 'https:' + imgSrc;
-        //         }
-        //         let imgPath="/"+i+"."+imgSrc.split(".").pop();
-        //         request(imgSrc).pipe(fs.createWriteStream(__dirname + "/imgs"+imgPath)); 
-        //     }
-        //   })
-    }
-})
+// download(url,function (data) {  
+//     if(data){
+//         let $=cheerio.load(data);
+//         // let imgs = $('img');
+//         console.log($('window.server'));
+//         console.log($('table').eq(1).find('script').eq(0).html());
+//         // $('img').each(function (i,elem) {
+//         //     let imgSrc = $(this).attr('src');
+//         //     console.log(imgSrc,1);
+//         //     if(imgSrc && i == 2){
+//         //         if(imgSrc.indexOf('https') == -1){
+//         //             imgSrc = 'https:' + imgSrc;
+//         //         }
+//         //         let imgPath="/"+i+"."+imgSrc.split(".").pop();
+//         //         request(imgSrc).pipe(fs.createWriteStream(__dirname + "/imgs"+imgPath)); 
+//         //     }
+//         //   })
+//     }
+// })
+
--- spider/phantomJsSpider.js
+++ spider/phantomJsSpider.js
@@ -1,20 +1,32 @@
 var webpage = require('webpage');
 var page = webpage.create();
+var pageInfoArray = [];
+
+//open为异步方法
 page.open('http://comic.kukudm.com/comiclist/3/3/1.htm',function(status){
-    var data;
     if(status === 'fail'){
-        console.log('open page fail!')
+        console.log('打开页面失败')
     }else{
-        // console.log(page.content);
-        var url = '';
-        url = page.evaluate(function(){
+        console.log('打开页面成功');
+        //页面信息数组
+        pageInfoArray = page.evaluate(function(){
+            //获取baseUrl
             var table = document.getElementsByTagName('table')[1];
             var img = table.getElementsByTagName('img')[0];
             var src = img.getAttribute('src');
-            return src;
+            src = src.slice(0,-6);
+            //获取总页数
+            var td = table.getElementsByTagName('td')[0];
+            var tdFirstChildText = td.firstChild.textContent;
+            var pageNum = tdFirstChildText.match(/共(\S*)页/)[1];
+            
+            return [src,pageNum];
         })
-        console.log(url);
+        // process.send(pageInfoArray);
     }
+    console.log('页面信息数组获取成功----',pageInfoArray);
     page.close();
     phantom.exit();
 });
+
+
