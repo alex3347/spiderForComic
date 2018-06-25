1bdcbd9716b8eb2ccec1df0af6673bd464867986
alex <alex@alexdeMacBook-Pro.local>
2018-06-25 18:45:51 +0800

    connect spider

--- spider/mainSpider.js
+++ spider/mainSpider.js
@@ -1,17 +1,18 @@
 const phantom = require('phantom');
 const fs = require('fs');
+const subPageSpider = require('./phantomNodeSpider.js').subPageSpider;
 
 let pageInfoArray = [];
-async function spider(url) {
+async function mainPageSpider(url) {
     //获取页面信息逻辑
     const instance = await phantom.create();
     const page = await instance.createPage();
     const status = await page.open(url);
 
     if(status === 'fail'){
-        console.log('打开页面失败')
+        console.log('打开主页面失败')
     }else{
-        console.log('打开页面成功');
+        console.log('打开主页面成功');
         //页面信息数组
         pageInfoArray = await page.evaluate(function(){
             //获取baseUrl
@@ -27,18 +28,27 @@ async function spider(url) {
             return temp;
         });
     }
-    console.log('页面链接数组获取成功----',pageInfoArray);
+    console.log('主页面链接数组获取成功');
     await instance.exit();
 
     //建立图片文件夹
-    for(let i = 0;i<pageInfoArray.length+1;i++){
+    // for(let i = 0;i<pageInfoArray.length+1;i++){
+        for(let i = 0;i<8;i++){
         fs.mkdir(`${__dirname}/imgs/${pageInfoArray[i]}`,function(err){  
             if(err)  
                 console.error(err);  
-            console.log('创建文件夹'+i+'成功');  
+            console.log('创建文件夹'+pageInfoArray[i]+'成功');  
         });
     }
+
+    //执行子页面爬取图片
+    function runSubPageSpider(pageInfoArray){
+        let arg = pageInfoArray[5];
+        let url = `http://comic.kukudm.com/comiclist/3/${arg}/1.htm`
+        subPageSpider(url,arg);
+    }
+    runSubPageSpider(pageInfoArray);
   }
 
-spider('http://comic.kukudm.com/comiclist/3/');
+mainPageSpider('http://comic.kukudm.com/comiclist/3/');
 
--- spider/phantomNodeSpider.js
+++ spider/phantomNodeSpider.js
@@ -3,16 +3,16 @@ const fs = require('fs');
 const request = require('request');
 
 let pageInfoArray = [];
-async function spider(url) {
+exports.subPageSpider = async function subPageSpider(url,folderName) {
     //获取页面信息逻辑
     const instance = await phantom.create();
     const page = await instance.createPage();
     const status = await page.open(url);
 
     if(status === 'fail'){
-        console.log('打开页面失败')
+        console.log('打开子页面'+ folderName +'失败')
     }else{
-        console.log('打开页面成功');
+        console.log('打开子页面'+ folderName +'成功');
         //页面信息数组
         pageInfoArray = await page.evaluate(function(){
             //获取baseUrl
@@ -28,25 +28,26 @@ async function spider(url) {
             return [src,pageNum];
         })
     }
-    console.log('页面信息数组获取成功----',pageInfoArray);
+    console.log('子页面信息数组获取成功----',pageInfoArray);
     await instance.exit();
 
     //图片下载逻辑
     let baseUrl = pageInfoArray[0];
     let maxNum = parseInt(pageInfoArray[1]);
-    for(let i = 1;i<maxNum+1;i++){
+    // for(let i = 1;i<maxNum+1;i++){
+        for(let i = 1;i<3;i++){
         if(i<10){
             i = '0'+i;
         }
         let requestUrl = baseUrl + i + '.jpg';
         let imgPath="/"+i+".jpg";
-        let writeStream = fs.createWriteStream(__dirname + "/imgs"+imgPath,{autoClose:true});
+        let writeStream = fs.createWriteStream(__dirname + "/imgs/"+ folderName + "/" + imgPath,{autoClose:true});
         request(requestUrl).pipe(writeStream); 
         writeStream.on('finish',function(){
-            console.log(i+'.jpg 保存成功')
+            console.log(folderName + "/" + i +'.jpg 保存成功')
         })
     }
   }
 
-spider('http://comic.kukudm.com/comiclist/3/3/1.htm');
+// subPageSpider('http://comic.kukudm.com/comiclist/3/3/1.htm');
 
